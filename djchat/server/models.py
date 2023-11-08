2b31b242379830026552ef995887bf3ef7d85c91
Hemant Umre <hemantumre12@gmail.com>
2023-11-08 17:56:33 +0530

    Developed chat Server Administration API.
    Completed with the initial backend and API set-up.

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,13 +1,52 @@
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+
+from .validators import validate_icon_image_size, validate_image_file_extension
+
+
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(
+        upload_to=category_icon_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_icon_image_size, validate_image_file_extension],
+    )
+
+    def save(self, *args, **kwargs):
+        # Filefield doesn't automatically delete a old file when updating
+        # with a new one, so here we manually do so.
+        # self.id is True when instance already exists and we're updating it.
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        icon_file = instance.icon
+        if icon_file:
+            icon_file.delete(save=False)
 
     def __str__(self):
         return self.name
+
+
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
+
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
 
 
 class Server(models.Model):
@@ -22,6 +61,38 @@ class Server(models.Model):
     )
     description = models.CharField(max_length=250, blank=True, null=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    banner = models.ImageField(
+        upload_to=server_banner_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_image_file_extension],
+    )
+    icon = models.ImageField(
+        upload_to=server_icon_upload_path,
+        null=True,
+        blank=True,
+        validators=[validate_icon_image_size, validate_image_file_extension],
+    )
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Server, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+
+        super(Server, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.server")
+    def server_delete_files(sender, instance, **kwargs):
+        icon_file = instance.icon
+        banner_file = instance.banner
+
+        if icon_file:
+            icon_file.delete(save=False)
+        if banner_file:
+            banner_file.delete(save=False)
 
     def __str__(self):
         return self.name
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,19 +1,53 @@
-from django.shortcuts import render
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
+from .schema import server_list_docs
 from .serializer import ServerSerializer
 
 
+@server_list_docs
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
     def list(self, request):
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+        by_serverID = request.query_params.get("by_serverID")
+        with_num_members = request.query_params.get("with_num_members")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        if by_user:
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
+
+        if by_serverID:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+            try:
+                self.queryset = self.queryset.filter(id=by_serverID)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        detail=f"Server with id {by_serverID} not found"
+                    )
+            except ValueError:
+                raise ValidationError(detail="Server value error")
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
+
+        serializer = ServerSerializer(
+            self.queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)
