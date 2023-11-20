c14835baf4370b2f857d5abaa8aabd43d7a2629f
Hemant Umre <hemantumre12@gmail.com>
2023-11-20 18:48:00 +0530

    API: Configure client/server to fetch data from the API.
    - Added Axios for making API calls from the client side.
    - Integrated django-cors-headers for enabling Cross-Origin
    Resource Sharing (CORS).

--- reactchat/src/pages/templates/SecondaryDraw.tsx
+++ reactchat/src/pages/templates/SecondaryDraw.tsx
@@ -1,8 +1,20 @@
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import axios from "axios";
 
 const SecondaryDraw = () => {
   const theme = useTheme();
+
+  // Trying pulling data from API
+  axios
+    .get("http://127.0.0.1:8000/api/server/select/?category=cat-1")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <Box
       sx={{
