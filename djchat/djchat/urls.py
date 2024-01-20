d0b4c56c9b7814db88d71e3401f769c138eed0da
Hemant Umre <hemantumre12@gmail.com>
2024-01-20 20:11:33 +0530

    auth: Set-up Simple JWT for authentication.

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from server.views import CategoryListViewSet, ServerListViewSet
 from webchat.consumers import WebChatConsumer
 from webchat.views import MessageViewSet
@@ -18,6 +22,12 @@ urlpatterns = [
     path("api/docs/schema", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/schema/ui", SpectacularSwaggerView.as_view(url_name="schema")
+    ),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
     ),
 ] + rounter.urls
 
