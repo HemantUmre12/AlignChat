fd8266f45967da5faafc3ebb90d81e0367fd84d4
Hemant Umre <hemantumre12@gmail.com>
2024-01-20 16:58:36 +0530

    message_interface: Build the UI.
    Built the message interface UI with messages and a textarea
    to send the message. The messages are scrolled to the latest
    message using reverse scrolling. Also, added the date and
    time alongside the sender's name.

--- reactchat/src/components/Main/MessageInterface.tsx
+++ reactchat/src/components/Main/MessageInterface.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import useWebSocket from "react-use-websocket";
 import useCrud from "../../hooks/useCrud";
 import { Server } from "../../@type/server";
 import MessageInterfaceChannels from "./MessageInterfaceChannels";
+import {
+  Avatar,
+  Box,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Scroll from "./Scroll";
 
 interface Message {
   id: number;
@@ -11,12 +23,20 @@ interface Message {
   timestamp: string;
 }
 
+interface SendMessageData {
+  type: string;
+  message: string;
+  // [key: string]: any;
+}
+
 interface Props {
   activeServer: Server;
   channelId: string;
 }
 
 const MessageInterface: React.FC<Props> = (props) => {
+  const theme = useTheme();
+
   const [newMessage, setNewMessage] = useState<Message[]>([]);
   const [message, setMessage] = useState("");
   const { fetchData } = useCrud<Message>(
@@ -33,27 +53,56 @@ const MessageInterface: React.FC<Props> = (props) => {
         const data = await fetchData();
         setNewMessage([]);
         setNewMessage(Array.isArray(data) ? data : []);
-        console.log("Connected🔗");
+        console.log("Connected 🔗");
       } catch (error) {
         console.log(error);
       }
     },
     onClose: () => {
-      console.log("Closed🚫");
+      console.log("Closed 🚫");
     },
     onError: () => {
-      console.log("Error☢️");
+      console.log("Error ☢️");
     },
     onMessage: (msg) => {
       const data = JSON.parse(msg.data);
       const new_message = data.new_message;
       setNewMessage((prevMessage) => [...prevMessage, new_message]);
+      setMessage("");
     },
   });
 
-  const handleSendMessage = () => {
-    sendJsonMessage({ type: "message", message });
-    setMessage("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendJsonMessage({
+      type: "message",
+      message,
+    } as SendMessageData);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      sendJsonMessage({
+        type: "message",
+        message,
+      } as SendMessageData);
+    }
+  };
+
+  const formatTimeStamp = (timestamp: string): string => {
+    const date = new Date(Date.parse(timestamp));
+    const formattedDate = `${date.getDate()}/${
+      date.getMonth() + 1
+    }/${date.getFullYear()}`;
+
+    const formattedTime = date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    return `${formattedDate} at ${formattedTime}`;
   };
 
   return (
@@ -63,28 +112,95 @@ const MessageInterface: React.FC<Props> = (props) => {
         channelId={props.channelId}
       />
 
-      <div>
-        {newMessage.map((msg: Message, index: number) => {
-          return (
-            <div key={index}>
-              <p>{msg.content}</p>
-            </div>
-          );
-        })}
+      {/* Messages */}
+      <Scroll>
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          {newMessage.map((msg: Message, index: number) => {
+            return (
+              <ListItem key={index} alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="user image" />
+                </ListItemAvatar>
+                <ListItemText
+                  primaryTypographyProps={{
+                    fontSize: "12px",
+                    variant: "body2",
+                  }}
+                  primary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body1"
+                        color="text.primary"
+                        sx={{ display: "inline", fontW: 600 }}
+                      >
+                        {msg.sender}
+                      </Typography>
+                      <Typography
+                        component="span"
+                        variant="caption"
+                        color="textSecondary"
+                      >
+                        {" at "}
+                        {formatTimeStamp(msg.timestamp)}
+                      </Typography>
+                    </>
+                  }
+                  secondary={
+                    <>
+                      <Typography
+                        variant="body1"
+                        style={{
+                          overflow: "visible",
+                          whiteSpace: "normal",
+                          textOverflow: "clip",
+                        }}
+                        sx={{
+                          display: "inline",
+                          lineHeight: 1.2,
+                          fontWeight: 400,
+                          letterSpacing: "-0.2px",
+                        }}
+                        component="span"
+                        color="text.primary"
+                      >
+                        {msg.content}
+                      </Typography>
+                    </>
+                  }
+                />
+              </ListItem>
+            );
+          })}
+        </List>
+      </Scroll>
 
-        <form>
-          <label>
-            Enter message:
-            <input
-              type="text"
-              onChange={(e) => setMessage(e.target.value)}
+      {/* Textarea */}
+      <Box sx={{ position: "sticky", bottom: 0, width: "100%" }}>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            bottom: 0,
+            right: 0,
+            padding: "1rem",
+            backgroundColor: theme.palette.background.default,
+            zIndex: 1,
+          }}
+        >
+          <Box sx={{ display: "flex" }}>
+            <TextField
+              fullWidth
+              multiline
+              minRows={1}
+              maxRows={4}
+              sx={{ flexGrow: 1 }}
               value={message}
-            ></input>
-          </label>
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+            ></TextField>
+          </Box>
         </form>
-
-        <button onClick={handleSendMessage}>Send Message</button>
-      </div>
+      </Box>
     </>
   );
 };
