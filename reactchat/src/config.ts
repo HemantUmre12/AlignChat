61f4765c17da251ab0341a40ef801970a5d74645
Hemant Umre <hemantumre12@gmail.com>
2024-02-10 19:06:28 +0530

    deployment: Change the server IP.

--- reactchat/src/config.ts
+++ reactchat/src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://127.0.0.1:8000/api";
-export const MEDIA_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "http://157.245.158.180/api";
+export const MEDIA_URL = "http://157.245.158.180";
